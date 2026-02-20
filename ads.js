f29fd019081249c4b7400c8a9736e738a0bcf597
hoanandam-ux <hoanandam@gmail.com>
2026-02-21 00:41:07 +0700

    Delete ads.js

--- ads.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  adsTime: process.env.ADS_TIME || 30,
-  checkAds: (startTime) => {
-    const now = Date.now();
-    return (now - startTime) / 1000 >= module.exports.adsTime;
-  }
-};
